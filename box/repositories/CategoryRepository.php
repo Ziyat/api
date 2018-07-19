596c0c1dbc99c5ee24d010f5a77da51d599bdac0
Ziyat <termit_90@mail.ru>
2018-07-19 17:21:37 +0500

    fixed

--- box/repositories/CategoryRepository.php
+++ box/repositories/CategoryRepository.php
@@ -15,14 +15,14 @@ class CategoryRepository
         return $category;
     }
 
-    public function save(Category $category): void
+    public function save(Category $category)
     {
         if (!$category->save()) {
             throw new \RuntimeException('Saving error.');
         }
     }
 
-    public function remove(Category $category): void
+    public function remove(Category $category)
     {
         if (!$category->delete()) {
             throw new \RuntimeException('Removing error.');

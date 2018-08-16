82f643cff9bc89a2710bcb832e289c13fffc8454
Ziyat <termit_90@mail.ru>
2018-08-16 16:23:15 +0500

    fixed

--- api/tests/_data/token.php
+++ api/tests/_data/token.php
@@ -3,28 +3,28 @@ return [
     [
         'user_id' => 1,
         'token' => 'token-correct',
-        'expired_at' => time() + 3600
+        'expired_at' => time() + 3600 * 24 * 7
     ],
 
     [
         'user_id' => 2,
         'token' => 'token-expired',
-        'expired_at' => time() - 3600
+        'expired_at' => time() - 3600 * 24 * 7
     ],
 
     [
         'user_id' => 2,
         'token' => 'token-correct-id-2',
-        'expired_at' => time() + 3600
+        'expired_at' => time() + 3600 * 24 * 7
     ],
     [
         'user_id' => 3,
         'token' => 'token-correct-id-3',
-        'expired_at' => time() + 3600
+        'expired_at' => time() + 3600 * 24 * 7
     ],
     [
         'user_id' => 4,
         'token' => 'token-expired-id-4',
-        'expired_at' => time() - 3600
+        'expired_at' => time() - 3600 * 24 * 7
     ],
 ];
